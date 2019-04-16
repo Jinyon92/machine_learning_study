d776544fbd5f7306bdccc199b0476e4d52cd4f85
choijinhyun <chjh0902@naver.com>
2019-04-17 01:33:02 +0900

    enumerage 와 zip 활용

---
+++ python for machine learning/enumerate_zip.py
@@ -0,0 +1,13 @@
+alist = ['a1','a2','a3']
+blist = ['b1','b2','b3']
+
+for a,b in zip(alist, blist):
+    print(a,b)
+
+a,b,c = zip((1,2,3),(10,20,30),(100,200,300))
+print(a,b,c)
+
+print([sum(x) for x in zip((1,2,3),(10,20,30),(100,200,300))])
+
+for i,(a,b) in enumerate(zip(alist,blist)):
+    print(i,a,b)
