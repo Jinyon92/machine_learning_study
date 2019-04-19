36c167e91c1ad2cfd41872ea5636f35bd49d08b1
choijinhyun <chjh0902@naver.com>
2019-04-19 16:06:27 +0900

    asterisk 활용

---
+++ python for machine learning/asterisk.py
@@ -0,0 +1,48 @@
+def asterisk_test(a, *args):
+    print(a, args)
+    print(type(args))
+
+asterisk_test(1,2,3,4,5,6)
+#1,(2,3,4,5,6)
+#tuple
+
+def asterisk_test(a, **kargs):
+    print(a, kargs)
+    print(type(kargs))
+
+asterisk_test(1,b=2,c=3,d=4,e=5,f=6)
+#1, {'b':2, 'c':3, 'd':4, 'e':5, 'f':6}
+#dict
+
+def asterisk_test(a, *args):
+    print(a, args[0])
+    print(type(args))
+
+asterisk_test(1,(2,3,4,5,6),(7,8,9))
+#1,(2,3,4,5,6)
+#tuple
+
+def asterisk_test(a, args):
+    print(a, *args)
+    print(type(args))
+
+asterisk_test(1,(2,3,4,5,6))
+#1,2,3,4,5,6
+#tuple
+
+a,b,c = ([1,2],[3,4],[5,6])
+print(a,b,c)
+
+data = ([1,2],[3,4],[5,6])
+print(*data)
+
+for data in zip(*([1,2],[3,4],[5,6])):
+    print(sum(data))
+#9
+#12
+
+def asterisk_test(a,b,c,d,e=0):
+    print(a,b,c,d,e)
+data = {"b":1, "c":2, "d":3, "e":56}
+asterisk_test(10, **data)
+#10 1 2 3 56
